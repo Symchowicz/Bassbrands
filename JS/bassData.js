fbc3522e0cd31bf1152569a698e2453f0e9fd4f7
Giselle MATIAS <giselle.magalong@gmail.com>
2021-05-15 16:51:17 +0200

    Ajout des photos + Création json

---
+++ JS/bassData.js
@@ -0,0 +1,113 @@
+var bass=[
+    // ---------------- FENDER -----------------
+    {
+        "image": "../pics/Fender/PlasmaRedBurst.JPG",
+        "marque": "Fender",
+        "nom": "FENDER AMERICAN ULTRA PRECISION BASS MN PLASMA RED BURST", //rouge - orange
+        "reference" : "306186",
+        "disponibilite" : "Pas de stock" ,
+        "garanti": "3ans",
+        "prix": "2299.00",
+        "prix conseille": "2309.00",
+        "nbVente":0,
+    },
+    {
+        "image": "../pics/Fender/DakotaRed.JPG",
+        "marque": "Fender",
+        "nom": "FENDER VINTERA '50S PRECISION BASS MN DAKOTA RED", //rouge - beige
+        "reference" : "306233",
+        "disponibilite" : "Pas de stock",
+        "garanti": "3ans",
+        "prix": "1069.00",
+        "prix conseille": "1099.00",
+        "nbVente":0,
+    },
+    {
+        "image": "../pics/Fender/PlasmaRedBurst.JPG",
+        "marque": "Fender",
+        "nom": "FENDER AMERICAN ULTRA PRECISION BASS MN ARCTIC PEARL", // blanc - marron
+        "reference" : "306184",
+        "disponibilite" : "Pas de stock",
+        "garanti": "3ans",
+        "prix": "2114.00",
+        "prix conseille": "2309.00",
+        "nbVente":0,
+    },
+    // ---------------- CORT -----------------
+    {
+        "image": "../pics/Cort/GlossBlack.JPG",
+        "marque": "Cort",
+        "nom": "CORT GB34JJ GLOSS BLACK", // noir
+        "reference" : "255792",
+        "disponibilite" : "disponible",
+        "garanti": "3ans",
+        "prix": "349.00",
+        "prix conseille": "349.00",
+        "nbVente":0,
+    },
+    {
+        "image" : "../pics/Cort/NaturalGloss.JPG",
+        "marque": "Cort",
+        "nom": "CORT GB64JJ NATURAL GLOSS", // bois
+        "reference" : "334237",
+        "disponibilite" : "disponible",
+        "garanti": "3ans",
+        "prix": "545.00",
+        "prix conseille": "569.00",
+        "nbVente":0,
+    },
+    {
+        "image": "../pics/Cort/PlacidBlue.JPG",
+        "marque": "Cort",
+        "nom": "CORT GB74 GIG LAKE PLACID BLUE", //bleue clair
+        "reference" : "305746",
+        "disponibilite" : "disponible",
+        "garanti": "3ans",
+        "prix": "759.00",
+        "prix conseille": "759.00",
+        "nbVente":0,
+    },
+    {
+        "image" : "../pics/Cort/CherryRed.JPG",
+        "marque": "Cort",
+        "nom": "CORT A4 PLUS FMMH CHERRY RED OPEN PORES",
+        "reference" : "224401",
+        "disponibilite" : "disponible",
+        "garanti": "3ans",
+        "prix": "698.00",
+        "prix conseille": "939.00",
+        "nbVente":0,
+    },
+    // -------------------- GIBSON -----------------
+    {
+        "marque": "",
+        "nom": "",
+        "reference" : "",
+        "disponibilite" : "",
+        "garanti": "",
+        "prix": "",
+        "prix conseille": "",
+        "nbVente":0,
+    },
+    {
+        "marque": "",
+        "nom": "",
+        "reference" : "",
+        "disponibilite" : "",
+        "garanti": "",
+        "prix": "",
+        "prix conseille": "",
+        "nbVente":0,
+    },
+    {
+        "marque": "",
+        "nom": "",
+        "reference" : "",
+        "disponibilite" : "",
+        "garanti": "",
+        "prix": "",
+        "prix conseille": "",
+        "nbVente":0,
+    },
+
+]
