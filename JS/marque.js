5889ec34d477ca400bc375050bcfb0b7f5408279
Giselle MATIAS <giselle.magalong@gmail.com>
2021-05-20 11:25:39 +0200

    Style page marque en mobile seulement

--- JS/marque.js
+++ JS/marque.js
@@ -1,3 +1,4 @@
+var presentation = document.querySelector('.presentation');
 var section = document.querySelector(".produit");
 /*-- Appel du fichier JSON --*/
 let requestUrl = 'https://raw.githubusercontent.com/Symchowicz/Bassbrands/main/JS/bassData.json';
@@ -19,10 +20,12 @@ function brandFilter(jsonObj){
     console.log(url);
     if(url ==="http://localhost:63342/Bassbrands/Fender.html"){
         var result = data.filter(bass => bass.marque ==='Fender');
+        presentation.classList.add('fender');
         showBass(result)
     }
     else if(url === 'http://localhost:63342/Bassbrands/Cort.html'){
         let result = data.filter(bass => bass.marque ==='Cort');
+        presentation.classList.add('cort');
         showBass(result);
     }
 }
@@ -33,27 +36,47 @@ function showBass(bass){
         var bassImage = document.createElement('img');
         var myArticle = document.createElement('article');
         var libelle = document.createElement('h2');
+        var infos = document.createElement('div');
+        var infoPrix = document.createElement('div');
         var prix = document.createElement('p');
+        var prixInitial = document.createElement('p');
+        var infoDispo = document.createElement('div');
         var stock = document.createElement('p');
         var ref = document.createElement('p');
+
+        //Attribution de class
+        libelle.classList.add('nom');
+        infos.classList.add('infos');
+        infoPrix.classList.add('infoPrix')
+        prix.classList.add('prix');
+        prixInitial.classList.add('prixInitial');
+        infoDispo.classList.add('infoDispo');
+        stock.classList.add('stock');
+        ref.classList.add('reference');
 
         // contenu des balises
         bassImage.setAttribute('src', bass[i].image);
         libelle.textContent = bass[i].nom;
-        prix.textContent = bass[i].prix + '€ au lieu de '+ bass[i]['prix initial'] + '€';
+        prix.textContent = bass[i].prix + '€';
+        prixInitial.textContent = 'au lieu de '+ bass[i]['prix initial'] + '€';
         if (bass[i].disponible){
             stock.textContent = 'En stock';
         }else{
             stock.textContent = "Rupture de stock";
         }
-        ref.textContent = "Référence : " + bass[i].reference;
+        ref.textContent = "Réf : " + bass[i].reference;
 
         // Attribution balise à Arcticle
+
         myArticle.appendChild(bassImage);
         myArticle.appendChild(libelle);
-        myArticle.appendChild(prix);
-        myArticle.appendChild(stock);
-        myArticle.appendChild(ref)
+        infoPrix.appendChild(prix);
+        infoPrix.appendChild(prixInitial);
+        infoDispo.appendChild(stock);
+        infoDispo.appendChild(ref);
+        infos.appendChild(infoPrix);
+        infos.appendChild(infoDispo);
+        myArticle.appendChild(infos);
         section.appendChild(myArticle);
     }
 }
