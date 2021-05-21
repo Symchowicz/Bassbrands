b43cc41acbf2abff7d68dac1cac45a1eb087a095
Giselle MATIAS <giselle.magalong@gmail.com>
2021-05-21 18:53:41 +0200

    Debug

--- JS/article.js
+++ JS/article.js
@@ -1,20 +1,19 @@
-export function loadata(ref){
-    let requestUrl = 'https://raw.githubusercontent.com/Symchowicz/Bassbrands/main/JS/bassData.json';
-    let request = new XMLHttpRequest();
-    request.open('GET',requestUrl);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function (){
-        var data = request.response;
-        loadArticle(data[0]);
-        var i = 0
-        const bouton = document.getElementById('bouton_panier');
-        bouton.addEventListener('click', () => {
-            localStorage.setItem(localStorage.length++, JSON.stringify(data[i]));
-            console.log(localStorage)
-        });
-    }
+let requestUrl = 'https://raw.githubusercontent.com/Symchowicz/Bassbrands/main/JS/bassData.json';
+let request = new XMLHttpRequest();
+request.open('GET',requestUrl);
+
+request.responseType = 'json';
+request.send();
+request.onload = function (){
+    var data = request.response;
+    loadArticle(data[0]);
+    const bouton = document.getElementById('bouton_panier');
+    bouton.addEventListener('click', () => {
+        localStorage.setItem(localStorage.length++, JSON.stringify(data[0]));
+        console.log(localStorage)
+    });
 }
+
 function loadArticle(jsonObj){
     document.getElementById('cheminMarque').innerHTML = jsonObj.marque + ' >';
     document.getElementById('cheminNom').innerHTML = jsonObj.nom;
