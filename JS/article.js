1202b2718167ee254ad535fc1329aba1f3f58db6
Giselle MATIAS <giselle.magalong@gmail.com>
2021-05-28 22:28:18 +0200

    Add Caractéristiques

--- JS/article.js
+++ JS/article.js
@@ -1,3 +1,8 @@
+const sectionChemin = document.querySelector(".chemin");
+const sectionDesc = document.querySelector(".descArticle");
+const sectionDetails = document.querySelector(".details");
+
+
 export function article(reference){
     let requestUrl = 'https://raw.githubusercontent.com/Symchowicz/Bassbrands/main/JS/bassData.json';
     let request = new XMLHttpRequest();
@@ -10,7 +15,8 @@ export function article(reference){
         var bassindex = data.findIndex(bass => bass.reference === reference)
         var bassArticle = data[bassindex];
         loadArticle(bassArticle);
-        const bouton = document.getElementById('bouton_panier');
+        loadCaracteristique(bassArticle)
+        const bouton = document.querySelector('.btnPanier');
         bouton.addEventListener('click', () => {
             //TODO Systéme de clé incrementation
             let azs = localStorage.getItem(bassArticle.reference)
@@ -23,19 +29,117 @@ export function article(reference){
                 localStorage.setItem(bassArticle.reference, JSON.stringify(bassArticle));
             }
         });
+        const voirPlus = document.querySelector('.voirPlus');
+        voirPlus.addEventListener('click', ()=>{
+            if (sectionDetails.style.display === "none") {
+                voirPlus.textContent = "Voir moins";
+                sectionDetails.style.display = "block";
+            } else {
+                voirPlus.textContent = "Voir les caractéristiques";
+                sectionDetails.style.display = "none";
+            }
+        });
 
     }
 }
 
 
 function loadArticle(jsonObj){
-    document.getElementById('cheminMarque').innerHTML = jsonObj.marque + ' >';
-    document.getElementById('cheminNom').innerHTML = jsonObj.nom;
-    var image = document.getElementById('Bass')
-    image.setAttribute('src', jsonObj.image);
-    image.setAttribute('alt', jsonObj.nom);
-    document.querySelector('h1').innerHTML = jsonObj.nom;
-    document.getElementById('description').innerHTML = jsonObj.description;
-    document.getElementById('prix').innerHTML = jsonObj.prix + "€";
-    document.getElementById('prix_initial').innerHTML = 'au lieu de ' + jsonObj['prix initial'] + "€";
+    let cheminBass = document.createElement('a');
+    let imgBass = document.createElement('img');
+    let articleBass = document.createElement('article');
+    let divDesc = document.createElement('div');
+    let nomBass = document.createElement('h1');
+    let titre = document.createElement('h2');
+    let description = document.createElement('p');
+    let divPrixBtn = document.createElement('div');
+    let prix = document.createElement('span');
+    let button = document.createElement('button');
+    let iconPanier = document.createElement('img');
+    let textBtn = document.createElement('span');
+
+    imgBass.classList.add('imgBass');
+    divDesc.classList.add('divDesc');
+    nomBass.classList.add('titre');
+    titre.classList.add('titre');
+    divPrixBtn.classList.add('divPrixBtn');
+    button.classList.add('btnPanier');
+
+
+    cheminBass.setAttribute('href', window.location );
+    imgBass.setAttribute('src', jsonObj.image);
+    iconPanier.setAttribute('src', './pics/panier_noir.svg');
+    cheminBass.textContent = jsonObj.nom;
+    nomBass.textContent = jsonObj.nom;
+    titre.textContent = "Description";
+    description.textContent = jsonObj.description;
+    prix.textContent = jsonObj.prix+ "au lieu de "+ jsonObj['prix initial']+"€";
+    textBtn.textContent = "Ajouter";
+
+
+    sectionChemin.appendChild(cheminBass);
+    sectionDesc.appendChild(imgBass);
+    sectionDesc.appendChild(articleBass);
+    divDesc.appendChild(nomBass);
+    divDesc.appendChild(titre);
+    divDesc.appendChild(description);
+    articleBass.appendChild(divDesc);
+    divPrixBtn.appendChild(prix);
+    button.appendChild(textBtn);
+    button.appendChild(iconPanier);
+    divPrixBtn.appendChild(button);
+    articleBass.appendChild(divPrixBtn);
+
+}
+
+function loadCaracteristique(jsonObj){
+    let titreCarac = document.createElement('h1');
+    let divArticleCarac = document.createElement('div');
+    let article1 = document.createElement('article');
+    let article2 = document.createElement('article');
+    let corps = document.createElement('span');
+    let manche = document.createElement('span');
+    let touche = document.createElement('span');
+    let nbFrettes = document.createElement('span');
+    let chevalet = document.createElement('span');
+    let mecanique = document.createElement('span');
+    let micro = document.createElement('span');
+
+    divArticleCarac.classList.add('divArticleCarac');
+    article1.classList.add('article1');
+    article2.classList.add('article2');
+    titreCarac.textContent = "Caractéristiques :";
+    corps.textContent ="Corps :"+ jsonObj.corps;
+    manche.textContent ="Manche :"+ jsonObj.manche;
+    touche.textContent ="Touche :"+ jsonObj.touche;
+    nbFrettes.textContent ="Nombre de frettes :"+ jsonObj['nombre de frettes'];
+    chevalet.textContent ="Chevalet :"+ jsonObj.chevalet;
+    mecanique.textContent ="Mécanique :"+ jsonObj.mecanique;
+    micro.textContent ="Micro :"+ jsonObj.micro;
+
+    sectionDetails.appendChild(titreCarac);
+
+    if(jsonObj.marque ==="Fender"){
+        let btnCommande = document.createElement('span');
+        let controle = document.createElement('span');
+        let orientation = document.createElement('span');
+
+        btnCommande.textContent ="Bouton de commande :"+ jsonObj["bouton de commande"];
+        controle.textContent ="Controle :"+ jsonObj.controle;
+        orientation.textContent ="Orientation :"+ jsonObj.orientation;
+
+        article1.appendChild(corps);
+        article1.appendChild(manche);
+        article1.appendChild(touche);
+        article1.appendChild(nbFrettes);
+        article1.appendChild(chevalet);
+        article2.appendChild(mecanique);
+        article2.appendChild(micro);
+        article2.appendChild(btnCommande);
+        article2.appendChild(controle);
+        article2.appendChild(orientation);
+        divArticleCarac.appendChild(article1);
+        divArticleCarac.appendChild(article2);
+        sectionDetails.appendChild(divArticleCarac);
+    }
 }
